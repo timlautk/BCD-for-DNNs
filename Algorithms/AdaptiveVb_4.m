79f8e9a031ce61094701be27a2868c96df111874
Tim Lau <timlautk@gmail.com>
2018-01-26 22:29:03 +0800

    Add files via upload

--- Algorithms/AdaptiveVb_4.m
+++ Algorithms/AdaptiveVb_4.m
@@ -1,10 +1,10 @@
 function [W2new,b2new,beta1] = AdaptiveVb_4(lambda,gamma,y,a1,W2,W2star,b2,b2star,beta,t)
     W2new = W2 + beta*(W2star-W2);
     b2new = b2 + beta*(b2star-b2);
-    if gamma*norm(W2star*a1+b2star-y,'fro')^2+lambda*norm(W2star,'fro')^2 <= gamma*norm(W2new*a1+b2new-y,'fro')^2+lambda*norm(W2new,'fro')^2
+    if gamma/2*norm(W2star*a1+b2star-y,'fro')^2+lambda*norm(W2star,'fro')^2 <= gamma/2*norm(W2new*a1+b2new-y,'fro')^2+lambda*norm(W2new,'fro')^2
         beta1 = t*beta;
-%         W2new = W2star;
-%         b2new = b2star;
+        W2new = W2star;
+        b2new = b2star;
     else
 %         beta1 = min(beta/t,1);
         beta1 = beta;
--- Algorithms/AdaptiveWb1_3.m
+++ Algorithms/AdaptiveWb1_3.m
@@ -1,13 +1,13 @@
-function [W1new,b1new,beta7] = AdaptiveWb1_3(a0,a1,W1,W1star,b1,b1star,beta,t)
+function [W1new,b1new,beta1] = AdaptiveWb1_3(lambda,gamma,a0,a1,W1,W1star,b1,b1star,beta,t)
     W1new = W1 + beta*(W1star-W1);
     b1new = b1 + beta*(b1star-b1);
-    if norm(W1star*a0+b1star-a1,'fro')^2 <= norm(W1new*a0+b1new-a1,'fro')^2
-        beta7 = t*beta;
-%         W1new = W1star;
-%         b1new = b1star;
+    if gamma/2*norm(W1star*a0+b1star-a1,'fro')^2+lambda*norm(W1star,'fro')^2 <= gamma/2*norm(W1new*a0+b1new-a1,'fro')^2+lambda*norm(W1new,'fro')^2
+        beta1 = t*beta;
+        W1new = W1star;
+        b1new = b1star;
     else
-        beta7 = min(beta/t,1);
-%         beta7 = beta;
+%         beta1 = min(beta/t,1);
+        beta1 = beta;
     end
     
 end
